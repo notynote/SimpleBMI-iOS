55c685e1bb92946f7287acc400cf17b7ce260eae
notynote <32334875+notynote@users.noreply.github.com>
2020-10-21 11:21:23 +0700

    Initial Commit

---
+++ SimpleBMI/SimpleBMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBMIApp.swift
+//  SimpleBMI
+//
+//  Created by Pathompong Phongsaporamut on 21/10/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
