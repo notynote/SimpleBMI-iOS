0fda6decd02ddd5728d3b3d4a8d7ed18f59ae679
notynote <32334875+notynote@users.noreply.github.com>
2020-10-21 12:20:33 +0700

    Update ContentView.swift
    Finalize my app

--- SimpleBMI/ContentView.swift
+++ SimpleBMI/ContentView.swift
@@ -7,11 +7,64 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
+}
+
+struct ContentView: View {
+    
+    @State var resultText:String = "A Simple BMI Calculator"
+    @State var weight:String = ""
+    @State var height:String = ""
+    
+    var body: some View {
+        VStack {
+            Text(resultText)
+                .multilineTextAlignment(.center)
+                .padding()
+                .font(.largeTitle)
+            
+            HStack {
+                Text("Weight")
+                TextField("Kilograms", text: $weight)
+                    .keyboardType(.numberPad)
+            }.padding()
+            
+            HStack {
+                Text("Height")
+                TextField("Centimeters", text: $height)
+                    .keyboardType(.numberPad)
+            }.padding()
+            
+            Button(action: {
+                
+                self.endEditing()
+                
+                if (weight == "" || height == "") {
+                    
+                    resultText = "Please enter both Weight and Height"
+                    
+                } else {
+                    
+                    let result = Double(weight)! / pow((Double(height)!/100.0), 2)
+                    
+                    
+                    resultText = "Your BMI\n" + String(format: "%.2f", result)
+                    
+                }
+                
+            }) {
+                Text("Calculate")
+            }.padding()
+        }.padding()
+    }
+    
+    private func endEditing() {
+        UIApplication.shared.endEditing()
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
